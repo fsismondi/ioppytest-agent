cc56c896112b3b7197d570c12a7721e2cbc2511f
Federico Sismondi <federicosismondi@gmail.com>
2017-05-30 18:25:43 +0200

    support for no_ipv6_forwarding on OS running agent

--- utils/bootstrap_agent.py
+++ utils/bootstrap_agent.py
@@ -3,6 +3,7 @@ import sys
 import os
 import json
 import pika
+import argparse
 import time
 
 logging.getLogger('pika').setLevel(logging.INFO)
@@ -13,12 +14,13 @@ queue_name = 'unittest_packet_router'
 AMQP_URL = str(os.environ['AMQP_URL'])
 AMQP_EXCHANGE = str(os.environ['AMQP_EXCHANGE'])
 
-def publish_tun_start(channel, agent_id, ipv6_host, ipv6_prefix):
+def publish_tun_start(channel, agent_id, ipv6_host, ipv6_prefix, ipv6_no_forwarding = False):
     d = {
         "_type": "tun.start",
         "name": agent_id,
         "ipv6_host": ipv6_host,
         "ipv6_prefix": ipv6_prefix,
+        "ipv6_no_forwarding" : ipv6_no_forwarding
     }
 
     channel.basic_publish(
@@ -48,22 +50,25 @@ def check_response(channel,queue_name, agent_id):
 if __name__ == "__main__":
 
     try:
-        agent_id = sys.argv[1]
-        ipv6_prefix = sys.argv[2]
-        ipv6_host = sys.argv[3]
+        parser = argparse.ArgumentParser()
+        parser.add_argument("agent_name", help="Name of the agent")
+        parser.add_argument("ipv6_prefix", help="ipv6 prefix")
+        parser.add_argument("ipv6_host", help="ipv6 host network")
+        parser.add_argument("-nf", "--no_forwarding", help="activate or not IPv6 forwarding", action="store_true")
+        args = parser.parse_args()
+
+        agent_id = args.name
+        ipv6_prefix = args.ipv6_prefix
+        ipv6_host = args.ipv6_host
+        ipv6_no_forwarding = args.no_forwarding
+
 
     except:
-        print("Error, please execute as: python3 ...bootstrap_agent.py <agent_name>  <ipv6_prefix>  <ipv6_host>")
-        print("Example python3 ...bootstrap_agent.py coap_client_agent  bbbb  :1 ")
+        print("Error, please see help (-h)")
         sys.exit(1)
 
-
     connection = pika.BlockingConnection(pika.connection.URLParameters(AMQP_URL))
-
     channel = connection.channel()
-
-
-
     agent_event_q = 'agent_bootstrap'
     result = channel.queue_declare(queue=agent_event_q)
     callback_queue = result.method.queue
@@ -77,7 +82,7 @@ if __name__ == "__main__":
 
     for i in range (1,4):
         logging.debug("Let's start the bootstrap the agent %s try number %d" %(agent_id,i))
-        publish_tun_start(channel, agent_id, ipv6_host, ipv6_prefix)
+        publish_tun_start(channel, agent_id, ipv6_host, ipv6_prefix, ipv6_no_forwarding)
         time.sleep(4)
         if check_response(channel,agent_event_q, agent_id):
             logging.debug("Agent tun bootstrapped")
