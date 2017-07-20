1372abd5c847e17bfb7ca487055b202ecd1af3fc
Federico Sismondi <federicosismondi@gmail.com>
2017-07-20 18:31:11 +0200

    lil' refact

--- utils/serial_listener.py
+++ utils/serial_listener.py
@@ -1,13 +1,11 @@
+# -*- coding: utf-8 -*-
+
 import sys
 import pika
 import json
 import serial
 import os
-import time
 from collections import OrderedDict
-from binascii import unhexlify
-import time
-
 
 STATE_OK = 0
 STATE_ESC = 1
@@ -16,8 +14,9 @@ SLIP_END = 'c0'
 SLIP_ESC = 'db'
 SLIP_ESC_END = 'dc'
 SLIP_ESC_ESC = 'dd'
-class ReadCOM(object):
 
+
+class SerialListener(object):
     def __init__(self, dev, br, name, server, session, user, passwd):
         self.dev = dev
         self.br = br
@@ -29,23 +28,22 @@ class ReadCOM(object):
         self.frame = ''
         self.start_frame = 0
         self.amqp_exchange = str(os.environ['AMQP_EXCHANGE'])
-        self.state=STATE_OK
+        self.state = STATE_OK
         self.frame_slip = ''
-        self.ser = serial.Serial(
-            port=dev, \
-            baudrate=int(br), \
-            timeout=0.001)
-        self.ser.flushInput();
+        self.ser = serial.Serial(port=dev,
+                                 baudrate=int(br),
+                                 timeout=0.001
+                                 )
+        self.ser.flushInput()
 
         self.mrkey = "data.serial.fromAgent." + name
 
         credentials = pika.PlainCredentials(user, passwd)
         connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host=server, \
-            virtual_host=session, \
+            host=server,
+            virtual_host=session,
             credentials=credentials))
         self.ch = connection.channel()
-
 
     def state_rubbish(self, data):
         if data.encode('hex') == SLIP_END:
@@ -53,6 +51,7 @@ class ReadCOM(object):
             self.start_frame = 0
             self.frame = ''
             self.frame_slip = ''
+
     def state_esc(self, data):
         if data.encode('hex') != SLIP_ESC_END and data.encode('hex') != SLIP_ESC_ESC:
             self.state = STATE_RUBBISH
@@ -66,25 +65,25 @@ class ReadCOM(object):
                 elif data.encode('hex') == SLIP_ESC_END:
                     self.frame += SLIP_END
 
-    def state_ok(self,data):
+    def state_ok(self, data):
         if data.encode('hex') == SLIP_ESC:
             self.state = STATE_ESC
         else:
             if data.encode('hex') == SLIP_END:
                 if self.start_frame == 0:
-                #start frame
+                    # start frame
                     self.start_frame = 1
                     self.frame = ''
                 else:
-                #end frame
+                    # end frame
                     self.start_frame = 0
-                    self.send_amqp(self.frame,self.frame_slip)
+                    self.send_amqp(self.frame, self.frame_slip)
                     self.state = STATE_OK
             else:
-                    if self.start_frame == 1:
-                        self.frame += data.encode('hex')
-                    else:
-                        self.state = STATE_RUBBISH
+                if self.start_frame == 1:
+                    self.frame += data.encode('hex')
+                else:
+                    self.state = STATE_RUBBISH
 
     def recv_chars(self, chars):
         if chars:
@@ -99,25 +98,24 @@ class ReadCOM(object):
                 if self.state == STATE_OK:
                     self.state_ok(c)
 
-    def convert_bytearray_to_intarray(self,ba):
+    def convert_bytearray_to_intarray(self, ba):
         ia = []
         for e in ba:
             ia.append(e)
         return ia
 
-
-    def send_amqp(self, data,data_slip):
+    def send_amqp(self, data, data_slip):
         body = OrderedDict()
         body['_type'] = 'packet.sniffed.raw'
         # body['data'] = data
         # body['data_slip'] = data_slip
         body['data'] = self.convert_bytearray_to_intarray(bytearray.fromhex(data))
         body['data_slip'] = self.convert_bytearray_to_intarray(bytearray.fromhex(data_slip))
-        self.frame_slip=''
-        self.ch.basic_publish(exchange=self.amqp_exchange, \
-                              routing_key=self.mrkey, \
-                              body=json.dumps(body), )
-
+        self.frame_slip = ''
+        self.ch.basic_publish(exchange=self.amqp_exchange,
+                              routing_key=self.mrkey,
+                              body=json.dumps(body),
+                              )
 
     def run(self):
         while True:
@@ -127,13 +125,13 @@ class ReadCOM(object):
                 self.recv_chars(output)
 
 
-# p=ReadCOM('/dev/ttyUSB0','115200','agent2','f-interop.rennes.inria.fr','session01','paul','iamthewalrus')
-p = ReadCOM(sys.argv[1], int(sys.argv[2]), sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
-while True:
-    numbytes = p.ser.inWaiting()
-    if numbytes > 0:
-        output = p.ser.read(numbytes)  # read output
-        p.recv_chars(output)
+# # p=ReadCOM('/dev/ttyUSB0','115200','agent2','f-interop.rennes.inria.fr','session01','paul','iamthewalrus')
+# p = ReadCOM(sys.argv[1], int(sys.argv[2]), sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
+# while True:
+#     numbytes = p.ser.inWaiting()
+#     if numbytes > 0:
+#         output = p.ser.read(numbytes)  # read output
+#         p.recv_chars(output)
 
 # print (sys.argv[1])
 # print (int(sys.argv[2]))
